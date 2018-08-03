96c799ead4a45dd26ec3c8547bf6df63cc2414c6
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-03 23:45:32 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,12 +5,14 @@ def initialize(dog_name, dog_breed)
   @dog_breed = dog_breed
 end
 
-def name=(new_dog_name)
+def dog_name=(new_dog_name)
   @dog_name = new_dog_name
 end
 
 def dog_name
   @dog_name
 end
+
+def dog_
 
 end
